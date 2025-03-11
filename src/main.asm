87726059d1b575076c50cb4db0f107c5156cd7da
kzueirf12345 <jaba0192837465@gmail.com>
2025-03-11 18:19:41 +0400

    print string in foo, with calculate size

--- src/main.asm
+++ src/main.asm
@@ -1,21 +1,49 @@
+section .data
+
+MAX_REG_VAL     equ 0xFFFFFFFF
+END_STR         equ 0x0
 section .text
 
 global _start
 
+;;; =========================================MAIN===================================================
+
 _start: 
-    mov rax, 0x01      ; write64 (rdi, rsi, rdx) ... r10, r8, r9
-    mov rdi, 1         ; stdout
-    mov rsi, Msg
-    mov rdx, MsgLen    ; strlen (Msg)
-    syscall
+    mov rsi, Vmsg                           ; string addr for print
+    call printf
     
     mov rax, 0x3C      ; exit64 (rdi)
     xor rdi, rdi
     syscall
 
+;;; =========================================FUNCS==================================================
+;;; ---------------------------------------------
+;;; Descript:   print string with \0 in end on concole
+;;; Entry:      rsi = address string  
+;;; Exit:       None
+;;; Destroy: 	rax, rcx, rdx, rsi, rdi
+;;; ---------------------------------------------
+printf:
+
+;;; calculate string size
+    mov rdi, rsi                            ; rdi - string address for find
+    mov rcx, MAX_REG_VAL                    ; rdx - counter string size
+    mov al, END_STR                         ; al - end string symbol (\0)
+    repne scasb
+
+;;; rdx - string size = MAX_REG_VAL - rcx
+    mov rdx, MAX_REG_VAL
+    sub rdx, rcx
+
+    mov rax, 0x1                            ; syscall print string
+    mov rdi, 1                              ; descriptor - stdout
+    syscall
+ret
+
+;;; =========================================DATA===================================================
 section .data
 
-Msg:    db "Hello, world!", 0xa
-MsgLen  equ $ - Msg
+Vmsg     db "Hello, world!", 0xa, END_STR
+Vmsg_len  equ $ - Vmsg
 
 section .bss
